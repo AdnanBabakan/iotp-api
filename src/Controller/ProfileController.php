dd6448d000dcf0b593ce826631637f22ac06bf05
Adnan Babakan <adnanbabakan.personal@hotmail.com>
2023-04-25 05:45:03 +0330

    Profile update added

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller;
 
+use AngryBytes\Hash\Hash;
+use AngryBytes\Hash\Hasher\Password;
 use App\Controller\interfaces\TokenAuthenticatedControllerInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('/x-user')]
 class ProfileController extends AbstractController implements TokenAuthenticatedControllerInterface
@@ -20,6 +24,38 @@ class ProfileController extends AbstractController implements TokenAuthenticated
             'id' => $user->getId(),
             'first_name' => $user->getFirstName(),
             'last_name' => $user->getLastName(),
+        ]);
+    }
+
+    #[Route('/', name: 'x_user_patch', methods: ['POST'])]
+    public function patch(Request $request, EntityManagerInterface $entityManager, ValidatorInterface $validator): JsonResponse
+    {
+        $user = $request->attributes->get('user');
+
+        $user->setFirstName($request->get('first_name'));
+        $user->setLastName($request->get('last_name'));
+
+        if($request->get('password')) {
+            $hasher = new Hash(new Password());
+
+            $user->setPassword($hasher->hash($request->get('password')));
+        }
+
+        $errors = $validator->validate($user);
+
+        if ($errors->count() > 0) {
+            return $this->json([
+                'status' => 'FAILED',
+                'message' => 'SOMETHING_WENT_WRONG'
+            ])->setStatusCode(422);
+        }
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->json([
+            'status' => 'SUCCESSFUL',
+            'message' => 'USER_UPDATED_SUCCESSFULLY'
         ]);
     }
 }
